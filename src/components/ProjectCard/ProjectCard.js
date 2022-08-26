1e4543b376ef565785a4899a37da685bb51818a8
Abdelrahman Ahmed <73317105+abdelrahman252@users.noreply.github.com>
2022-08-26 17:46:08 +0100

    Add files via upload

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,31 +1,32 @@
 import { Col } from "react-bootstrap";
 
-import { Github, Globe  } from 'react-bootstrap-icons';
-import "./ProjectCard.css"
+import { Github, Globe } from "react-bootstrap-icons";
+import "./ProjectCard.css";
 export const ProjectCard = ({ title, description, imgUrl, Glink, Llink }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-        <img src={imgUrl} />
-        
+        <img src={imgUrl} alt={title} />
+
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
         </div>
 
         <div className="links">
-
           <div className="left">
-            <a href={Glink} target="_blank"><Github size={40} /></a>
+            <a href={Glink} target="_blank">
+              <Github size={40} />
+            </a>
           </div>
           <div className="right">
-            <a href={Llink} target="_blank"><Globe size={40} /></a>
+            <a href={Llink} target="_blank">
+              <Globe size={40} />
+            </a>
           </div>
-
         </div>
-
       </div>
-    </Col >
-  )
-}
-export default ProjectCard
+    </Col>
+  );
+};
+export default ProjectCard;
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../../assets/img/contact-img.svg";
 import "animate.css";
