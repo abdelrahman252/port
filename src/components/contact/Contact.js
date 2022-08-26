131e000ab6a0e9963d0b647d1e4b5026128b4ac0
Abdelrahman Ahmed <73317105+abdelrahman252@users.noreply.github.com>
2022-08-26 18:57:16 +0100

    Add files via upload

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,4 +1,3 @@
-
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../../assets/img/contact-img.svg";
 import "animate.css";
@@ -7,50 +6,11 @@ import "./contact.css";
 import emailjs from "emailjs-com";
 
 const Contact = () => {
-  // const formInitialDetails = {
-  //   firstName: '',
-  //   lastName: '',
-  //   email: '',
-  //   phone: '',
-  //   message: ''
-  // }
-  // const [formDetails, setFormDetails] = useState(formInitialDetails);
-  // const [buttonText, setButtonText] = useState('Send');
-  // const [status, setStatus] = useState({});
-
-  // const onFormUpdate = (category, value) => {
-  //   setFormDetails({
-  //     ...formDetails,
-  //     [category]: value
-  //   })
-  // }
-
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   setButtonText("Sending...");
-  //   let response = await fetch("http://localhost:5000/contact", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json;charset=utf-8",
-  //     },
-  //     body: JSON.stringify(formDetails),
-  //   });
-  //   setButtonText("Send");
-  //   let result = await response.json();
-  //   setFormDetails(formInitialDetails);
-  //   if (result.code === 200) {
-  //     setStatus({ succes: true, message: 'Message sent successfully' });
-  //   } else {
-  //     setStatus({ succes: false, message: 'Something went wrong, please try again later.' });
-  //   }
-  // };
   const btn = document.getElementById("button");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
     btn.value = "Sending...";
-
     emailjs
       .sendForm(
         "service_96zcp44",
